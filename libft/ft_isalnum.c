1ae062b3840e96be4a7dc4aeb0fea399e8ffccc4
jimmy <jimmy@192-168-1-105.tpgi.com.au>
2021-08-25 16:10:47 +0930

    ft_isalnum ft_isascii

---
+++ libft/ft_isalnum.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "ft_isdigit.c"
+#include "ft_isalpha.c"
+
+int	ft_isalnum(int c)
+{
+	return(ft_isdigit(c) || ft_isalpha(c));
+}
---
+++ libft/ft_isascii.c
@@ -0,0 +1,4 @@
+int	ft_isascii(char c)
+{
+	return (c >= 0 && c <= 0177);
+}
