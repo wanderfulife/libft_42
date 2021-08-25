8f502bc8a486626435dd0f943e1e574b2f43c436
jimmy <jimmy@192-168-1-105.tpgi.com.au>
2021-08-25 12:08:08 +0930

    ft_isdigit

--- libft/ft_isalpha.c
+++ libft/ft_isalpha.c
@@ -1,16 +1,7 @@
-#include <stdio.h>
 #include "ft_isupper.c"
 #include "ft_islower.c"
 
 int	ft_isalpha(int c)
 {
 	return (ft_isupper(c) || ft_islower(c));
-}
-
-int main(void)
-{
-	printf("%d",ft_isalpha('K'));
-	printf("%d",ft_isalpha('k'));
-	printf("%d",ft_isalpha(0));
-	return (0);
 }
