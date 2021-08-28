5113c805a509e840cd5206e653474205fa50b388
wanderfulife <john.wander@hotmail.fr>
2021-08-28 14:23:27 +0930

    memmove.c

---
+++ libft/ft_memmove.c
@@ -0,0 +1,17 @@
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	if (!dst && !src)
+		return (dst);
+	if (src < dst)
+		while (len--)
+			d[len] = s[len];
+	else
+		while (len--)
+			*d++ = *s++;
+	return (dst);
+}
