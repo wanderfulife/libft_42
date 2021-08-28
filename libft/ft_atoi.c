84ed6fed8e53527da4f2e044057845165c26a44d
wanderfulife <john.wander@hotmail.fr>
2021-08-28 12:57:14 +0930

    ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -1,25 +1,23 @@
-int	ft_atoi(char *str)
+#include <stdio.h>
+int	ft_atoi(const char *str)
 {
 	int	neg;
-	int	i;
 	int	num;
 
-	i = 0;
 	neg = 1;
 	num = 0;
-	while (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\v'
-		|| str[i] == '\f' || str[i] == '\r')
-		i++;
-	while (str[i] == '-' || str[i] == '+')
+	while (*str == ' ' || *str == '\n' || *str == '\t' || *str == '\v'
+		|| *str == '\f' || *str == '\r')
+		str++;
+	while (*str == '-' || *str == '+')
 	{
-		if (str[i] == '-')
+		if (*str == '-')
 			neg *= -1;
-		i++;
+		str++;
 	}
-	while (str[i] >= 48 && str[i] <= 57)
+	while (*str >= '0' && *str <= '9')
 	{
-		num = num * 10 + (str[i] - 48);
-		i++;
+		num = num * 10 + (*str++ - '0');
 	}
 	return (num * neg);
 }
