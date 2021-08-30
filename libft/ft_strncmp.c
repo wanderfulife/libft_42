bb3bedef9e6152031f8be69aff82d53a7f0b27b2
wanderfulife <john.wander@hotmail.fr>
2021-08-30 23:06:55 +0930

    ft_strncmp.c

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -1,18 +1,7 @@
-int	ft_strncmp(char *s1, char *s2, unsigned int n)
+int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 {
-	unsigned int	c;
-
-	c = 0;
-	while (n > 0 && s1[c] != '\0' && s2[c] != '\0')
-	{
-		if (s1[c] != s2[c])
-			break ;
-		c++;
-		n--;
-	}
-	if (n == 0)
-	{
-		return (0);
-	}
-	return (s1[c] - s2[c]);
+	while ((*s1 || *s2) && n--)
+		if (*s1++ != *s2++)
+			return ((unsigned char)*--s1 - (unsigned char)*--s2);
+	return (0);
 }
