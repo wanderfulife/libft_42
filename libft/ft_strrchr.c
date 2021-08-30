2fa35601df5014c1d941d7ccf89f743379747edb
wanderfulife <john.wander@hotmail.fr>
2021-08-30 16:55:38 +0930

    ft_strrchr.c

---
+++ libft/ft_strrchr.c
@@ -0,0 +1,12 @@
+char *ft_strrchr(const char *s, int c)
+{
+	const char *str;
+	
+	str = s;
+	while (*s)
+		s++;
+	while (s >= str)
+		if (*s-- == (char)c)
+			return ((char *)s + 1);
+	return (NULL);
+}
